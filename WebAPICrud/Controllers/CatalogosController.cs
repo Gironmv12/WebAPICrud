d43b71306da550f0d25d1dc1f81e65413f0d898e
Gironmv12 <gironsan08918@gmail.com>
2024-03-20 16:30:37 -0600

    si

---
+++ WebAPICrud/Controllers/CatalogosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPICrud.Core.BL.Interfaces;
+
+namespace WebAPICrud.Controllers
+{
+    public class CatalogosController : Controller
+    {
+
+        private readonly ICatalogo _catalogoService;
+
+        [HttpGet("ObtenerProductos")]
+        public async Task<IActionResult> ObtenerProductos()
+        {
+            var resultado = await _catalogoService.ObtenerProductos();
+            return Ok(resultado);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAPICrud/Controllers/SeguridadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPICrud.Core.BL.Interfaces;
+
+namespace WebAPICrud.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
